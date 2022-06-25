099216752e56cf1980ce685cf2a3fe905c746160
Patrick Samson <plsamson@quadx.xyz>
2022-06-25 21:15:54 +0800

    Add db and typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TagModule } from './modules/tag.module';
 import { TodoModule } from './modules/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SampleModule } from './sample/sample.module';
+import { TagEntity } from './models/tag/tag.entity';
+import { TodoEntity } from './models/todo/todo.entity';
 
 @Module({
-  imports: [TodoModule, SampleModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [TagEntity, TodoEntity],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    TagModule,
+    TodoModule,
+    SampleModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

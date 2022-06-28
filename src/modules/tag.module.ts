17b6c2529a7c556b76833de06549f4dbf180f6eb
Patrick Samson <plsamson@quadx.xyz>
2022-06-29 05:28:18 +0800

    Add graphql resolvers

--- src/modules/tag.module.ts
+++ src/modules/tag.module.ts
@@ -3,10 +3,11 @@ import { TagEntity } from '../models/tag/tag.entity';
 import { TagController } from '../controllers/tag.controller';
 import { TagService } from '../services/tag.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagResolver } from '../resolvers/tag.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TagEntity])],
   controllers: [TagController],
-  providers: [TagService]
+  providers: [TagResolver, TagService]
 })
 export class TagModule {}
--- src/modules/todo.module.ts
+++ src/modules/todo.module.ts
@@ -3,10 +3,11 @@ import { TodoEntity } from '../models/todo/todo.entity';
 import { TodoController } from '../controllers/todo.controller';
 import { TodoService } from '../services/todo.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoResolver } from '../resolvers/todo.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TodoEntity])],
   controllers: [TodoController],
-  providers: [TodoService]
+  providers: [TodoResolver, TodoService]
 })
 export class TodoModule {}

05c17e5e877395cdd013e40235521d3e0f8c9526
Patrick Samson <plsamson@quadx.xyz>
2022-07-05 00:47:06 +0800

    Comment import statement

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TransformInterceptor } from './interceptors/transform.interceptor';
+// import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);

e08a2c8eac8d83ca9291244b7f3f080d9a6e2555
Patrick Samson <plsamson@quadx.xyz>
2022-07-06 02:39:01 +0800

    Update modules

--- src/modules/tag.module.ts
+++ src/modules/tag.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagController } from '../controllers/tag/tag.controller';
 import { TagDbController } from '../controllers/tag/tagDb.controller';
-import { TagNoDbController } from '../controllers/tag/tagNoDb.controller';
 import entities from '../models/index.entity';
 import tagProviders from '../providers/tag.provider';
 
 @Module({
   imports: [TypeOrmModule.forFeature(entities)],
-  controllers: [TagDbController, TagNoDbController],
+  controllers: [TagController, TagDbController],
   providers: tagProviders
 })
 export class TagModule {}
--- src/modules/todo.module.ts
+++ src/modules/todo.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoController } from '../controllers/todo/todo.controller';
 import { TodoDbController } from '../controllers/todo/todoDb.controller';
-import { TodoNoDbController } from '../controllers/todo/todoNoDb.controller';
 import entities from '../models/index.entity';
 import todoProviders from '../providers/todo.provider';
 
 @Module({
   imports: [TypeOrmModule.forFeature(entities)],
-  controllers: [TodoDbController, TodoNoDbController],
+  controllers: [TodoController, TodoDbController],
   providers: todoProviders
 })
 export class TodoModule {}

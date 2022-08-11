b7fdfd97a976dfae1b558ab686ea4ec000e103dc
Patrick Samson <plsamson@quadx.xyz>
2022-08-12 05:59:29 +0800

    Add data source

---
+++ src/data-source.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+import { DataSource } from "typeorm";
+
+dotenv.config();
+
+const dataSource: DataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  migrations: [join(process.cwd(), './src/migrations/*.ts')],
+  migrationsTableName: 'migrations',
+});
+
+export default dataSource;

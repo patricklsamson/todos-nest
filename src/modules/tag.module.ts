b459a45fedafec8bb4003b93eb7502c0528b5b06
Patrick Samson <plsamson@quadx.xyz>
2022-08-13 13:35:33 +0800

    Update modules

--- src/modules/tag.module.ts
+++ src/modules/tag.module.ts
@@ -4,10 +4,8 @@ import { TagController } from '../controllers/tag/tag.controller';
 import { TagDbController } from '../controllers/tag/tag-db.controller';
 import indexEntity from '../models/index.entity';
 import { TagDbResolver } from "../resolvers/tag/tag-db.resolver";
-import { TagNoDbResolver } from "../resolvers/tag/tag.resolver";
+import { TagResolver } from "../resolvers/tag/tag.resolver";
 import { TagDbService } from "../services/tag/tag-db.service";
-import { TagGqlDbService } from "../services/tag/tag-gql-db.service";
-import { TagGqlService } from "../services/tag/tag-gql.service";
 import { TagService } from "../services/tag/tag.service";
 
 @Module({
@@ -16,9 +14,7 @@ import { TagService } from "../services/tag/tag.service";
   providers: [
     TagDbResolver,
     TagDbService,
-    TagGqlDbService,
-    TagGqlService,
-    TagNoDbResolver,
+    TagResolver,
     TagService
   ]
 })
--- src/modules/todo.module.ts
+++ src/modules/todo.module.ts
@@ -4,10 +4,8 @@ import { TodoController } from '../controllers/todo/todo.controller';
 import { TodoDbController } from '../controllers/todo/todo-db.controller';
 import indexEntity from '../models/index.entity';
 import { TodoDbResolver } from '../resolvers/todo/todo-db.resolver';
-import { TodoNoDbResolver } from '../resolvers/todo/todo.resolver';
+import { TodoResolver } from '../resolvers/todo/todo.resolver';
 import { TodoDbService } from '../services/todo/todo-db.service';
-import { TodoGqlDbService } from '../services/todo/todo-gql-db.service';
-import { TodoGqlService } from '../services/todo/todo-gql.service';
 import { TodoService } from '../services/todo/todo.service';
 
 @Module({
@@ -16,9 +14,7 @@ import { TodoService } from '../services/todo/todo.service';
   providers: [
     TodoDbResolver,
     TodoDbService,
-    TodoGqlDbService,
-    TodoGqlService,
-    TodoNoDbResolver,
+    TodoResolver,
     TodoService
   ]
 })

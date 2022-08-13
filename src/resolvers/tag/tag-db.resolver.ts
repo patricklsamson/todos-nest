1db36bbce247a6328779c2c341f9a5b1effb8b57
Patrick Samson <plsamson@quadx.xyz>
2022-08-13 13:35:41 +0800

    Update tag resolvers

--- src/resolvers/tag/tag-db.resolver.ts
+++ src/resolvers/tag/tag-db.resolver.ts
@@ -3,39 +3,39 @@ import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { TagDb } from "../../models/tag/tag-db.entity";
 import { CreateTagInput } from '../../requests/tag/create-tag.input';
 import { UpdateTagInput } from '../../requests/tag/update-tag.input';
-import { TagGqlDbService } from '../../services/tag/tag-gql-db.service';
+import { TagDbService } from '../../services/tag/tag-db.service';
 
 @Resolver(() => TagDb)
 export class TagDbResolver {
-  constructor(private tagService: TagGqlDbService) {}
+  constructor(private tagService: TagDbService) {}
 
   @Query(() => [TagDb], { name: 'dbTags' })
   findAllDbTags(): Promise<TagDb[]> {
-    return this.tagService.findAllTags();
+    return this.tagService.findAll();
   }
 
   @Query(() => TagDb, { name: 'dbTag' })
   findOneDbTag(@Args('id', ParseIntPipe) id: number): Promise<TagDb> {
-    return this.tagService.findOneTag(id);
+    return this.tagService.findOne(id);
   }
 
   @Mutation(() => TagDb)
   createDbTag(@Args('tag') tag: CreateTagInput): Promise<TagDb> {
-    return this.tagService.createTag(tag);
+    return this.tagService.create(tag);
   }
 
   @Mutation(() => TagDb)
   updateDbTag(@Args('tag') tag: UpdateTagInput): Promise<TagDb> {
-    return this.tagService.updateTag(tag.id, tag);
+    return this.tagService.update(tag.id, tag);
   }
 
   @Mutation(() => Boolean)
   removeAllDbTags(): Promise<boolean> {
-    return this.tagService.removeAllTags();
+    return this.tagService.removeAll();
   }
 
   @Mutation(() => Boolean)
   removeOneDbTag(@Args('id', ParseIntPipe) id: number): Promise<boolean> {
-    return this.tagService.removeOneTag(id);
+    return this.tagService.removeOne(id);
   }
 }
--- src/resolvers/tag/tag.resolver.ts
+++ src/resolvers/tag/tag.resolver.ts
@@ -2,25 +2,25 @@ import { ParseIntPipe } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Tag } from '../../models/tag/tag.entity';
 import { TagInput } from '../../requests/tag/tag.input';
-import { TagGqlService } from '../../services/tag/tag-gql.service';
+import { TagService } from '../../services/tag/tag.service';
 
 @Resolver(() => Tag)
-export class TagNoDbResolver {
-  constructor(private tagService: TagGqlService) {}
+export class TagResolver {
+  constructor(private tagService: TagService) {}
 
   @Query(() => [Tag], { name: 'tags' })
   findAllTags(): Tag[] {
-    return this.tagService.findAllTags();
+    return this.tagService.findAll();
   }
 
   @Query(() => Tag, { name: 'tag' })
   findOneTag(@Args('id', ParseIntPipe) id: number): Tag {
-    return this.tagService.findOneTag(id);
+    return this.tagService.findOne(id);
   }
 
   @Mutation(() => Tag)
   createTag(@Args('tag') tag: TagInput): Tag {
-    return this.tagService.createTag(tag);
+    return this.tagService.create(tag);
   }
 
   @Mutation(() => Tag)
@@ -28,16 +28,16 @@ export class TagNoDbResolver {
     @Args('id', ParseIntPipe) id: number,
     @Args('tag') tag: TagInput
   ): Tag {
-    return this.tagService.updateTag(id, tag);
+    return this.tagService.update(id, tag);
   }
 
   @Mutation(() => Boolean)
   removeAllTags(): boolean {
-    return this.tagService.removeAllTags();
+    return this.tagService.removeAll();
   }
 
   @Mutation(() => Boolean)
   removeOneTag(@Args('id', ParseIntPipe) id: number): boolean {
-    return this.tagService.removeOneTag(id);
+    return this.tagService.removeOne(id);
   }
 }

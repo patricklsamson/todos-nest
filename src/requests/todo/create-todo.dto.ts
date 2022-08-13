76d32f8cfecf4a37a18303b7e96e72886cf8ce21
Patrick Samson <plsamson@quadx.xyz>
2022-08-13 13:46:15 +0800

    Delete unneeded todo requests

--- src/requests/todo/create-todo.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsBoolean, IsString } from "class-validator";
-
-export class CreateTodoDto {
-  @IsString()
-  body: string;
-
-  @IsBoolean()
-  done: boolean;
-}
--- src/requests/todo/update-todo.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { IsNumber } from "class-validator";
-import { CreateTodoDto } from "./create-todo.dto";
-
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {
-  @IsNumber()
-  id: number;
-}

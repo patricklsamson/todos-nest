df2ae07a35b63a3e78ce5d5aaccbe304ef35400b
Patrick Samson <plsamson@quadx.xyz>
2022-08-14 19:52:37 +0800

    Update tag controller

--- src/controllers/tag/tag-db.controller.ts
+++ src/controllers/tag/tag-db.controller.ts
@@ -14,7 +14,6 @@ import {
 import { TagDb } from '../../models/tag/tag-db.entity';
 import { CreateTagDbInput } from '../../requests/tag/create-tag-db.input';
 import { UpdateTagDto } from '../../requests/tag/update-tag.dto';
-import { TagSerializer } from '../../serializers/tag.serializer';
 import { TagDbService } from '../../services/tag/tag-db.service';
 
 @Controller('db-tags')
@@ -23,47 +22,35 @@ export class TagDbController {
   constructor(private tagService: TagDbService) {}
 
   @Get()
-  async findAll(): Promise<TagDb[]> {
-    return TagSerializer.serialize(await this.tagService.findAll());
+  findAll(): Promise<TagDb[]> {
+    return this.tagService.findAll();
   }
 
   @Get(':id')
-  async findOne(@Param('id', ParseIntPipe) id: number): Promise<TagDb> {
-    try {
-      return TagSerializer.serialize(await this.tagService.findOne(id));
-    } catch (err) {
-      throw new NotFoundException(err.message);
-    }
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<TagDb> {
+    return this.tagService.findOne(id);
   }
 
   @Post()
-  async create(@Body() tag: CreateTagDbInput): Promise<TagDb> {
-    return TagSerializer.serialize(await this.tagService.create(tag));
+  create(@Body() tag: CreateTagDbInput): Promise<TagDb> {
+    return this.tagService.create(tag);
   }
 
   @Put(':id')
-  async update(
+  update(
     @Param('id', ParseIntPipe) id: number,
     @Body() tag: UpdateTagDto
   ): Promise<TagDb> {
-    try {
-      return TagSerializer.serialize(await this.tagService.update(id, tag));
-    } catch (err) {
-      throw new NotFoundException(err.message);
-    }
+    return this.tagService.update(id, tag);
   }
 
   @Delete()
-  removeAll(): Promise<boolean> {
+  removeAll(): Promise<object> {
     return this.tagService.removeAll();
   }
 
   @Delete(':id')
-  async removeOne(@Param('id', ParseIntPipe) id: number): Promise<boolean> {
-    try {
-      return await this.tagService.removeOne(id);
-    } catch (err) {
-      throw new NotFoundException(err.message);
-    }
+  removeOne(@Param('id', ParseIntPipe) id: number): Promise<object> {
+    return this.tagService.removeOne(id);
   }
 }

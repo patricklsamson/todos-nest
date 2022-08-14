4720803cac4da926f52e67da9ef874de08ad0b56
Patrick Samson <plsamson@quadx.xyz>
2022-08-14 19:53:10 +0800

    Update tag service

--- src/services/tag/tag-db.service.ts
+++ src/services/tag/tag-db.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { TagDb } from "../../models/tag/tag-db.entity";
 import { CreateTagDbInput } from "../../requests/tag/create-tag-db.input";
 import { UpdateTagDto } from "../../requests/tag/update-tag.dto";
 import { UpdateTagInput } from "../../requests/tag/update-tag.input";
+import { TagSerializer } from "../../serializers/tag.serializer";
 import { RepositoryService } from "../repository.service";
 
 @Injectable()
@@ -15,40 +16,71 @@ export class TagDbService {
     });
   }
 
-  findOne(id: number): Promise<TagDb> {
-    return this.repositoryService.tagRepository.findOneOrFail({
-      where: { id: id },
-      relations: ['todo']
-    });
+  async findOne(id: number): Promise<TagDb> {
+    try {
+      const tag: TagDb =
+        await this.repositoryService.tagRepository.findOneOrFail({
+          where: { id: id },
+          relations: ['todo']
+        });
+
+      return TagSerializer.serialize(tag);
+    } catch (err) {
+      throw new NotFoundException(err.message);
+    }
   }
 
-  create(tag: CreateTagDbInput): Promise<TagDb> {
+  async create(tag: CreateTagDbInput): Promise<TagDb> {
     const newTag: CreateTagDbInput =
       this.repositoryService.tagRepository.create(tag);
 
-    return this.repositoryService.tagRepository.save(newTag);
+    return TagSerializer.serialize(
+      await this.repositoryService.tagRepository.save(newTag)
+    );
   }
 
   async update(id: number, tag: UpdateTagDto|UpdateTagInput): Promise<TagDb> {
-    await this.repositoryService.tagRepository.update(id, tag);
+    try {
+      await this.repositoryService.tagRepository.update(id, tag);
 
-    return this.repositoryService.tagRepository.findOneByOrFail({ id: id });
+      return TagSerializer.serialize(
+        await this.repositoryService.tagRepository.findOneByOrFail({ id: id })
+      );
+    } catch (err) {
+      throw new NotFoundException(err.message);
+    }
   }
 
-  async removeAll(): Promise<boolean> {
+  async removeAll(): Promise<object> {
     const tags: TagDb[] = await this.repositoryService.tagRepository.find();
 
     tags.forEach(tag => this.repositoryService.tagRepository.remove(tag));
 
-    return true;
+    return {
+      data: {
+        attributes: {
+          success: true
+        }
+      }
+    };
   }
 
-  async removeOne(id: number): Promise<boolean> {
-    const tag: TagDb =
-      await this.repositoryService.tagRepository.findOneByOrFail({ id: id });
+  async removeOne(id: number): Promise<object> {
+    try {
+      const tag: TagDb =
+        await this.repositoryService.tagRepository.findOneByOrFail({ id: id });
 
-    await this.repositoryService.tagRepository.remove(tag);
+      this.repositoryService.tagRepository.remove(tag);
 
-    return true;
+      return {
+        data: {
+          attributes: {
+            success: true
+          }
+        }
+      };
+    } catch (err) {
+      throw new NotFoundException(err.message);
+    }
   }
 }
